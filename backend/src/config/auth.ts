2259ccb0a423f14db6ef7994c331027f0ec811b1
IceHower <vctelles09@gmail.com>
2020-07-26 23:38:51 -0300

    node_modules be gone

--- backend/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    jwt: {
-        secret: 'e4a59898046b3ab79433efcdb135cd8b',
-        expireIn: '1d'
-    }
-}
--- backend/src/database/index.ts
+++
@@ -1,5 +0,0 @@
-import { createConnection } from 'typeorm';
-
-//Essa função busca pela projeto um arquivo ormconfig e pega as configurações estabelecidas lá, por isso não e necessario passar nenhum parametro.
-createConnection();
-
